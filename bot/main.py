c9fb9ad600a295441707b1cd9a36a10c35ad9d56
AS1 <arseniykofanov@gmail.com>
2025-09-25 21:33:10 +0300

    -

--- bot/main.py
+++ bot/main.py
@@ -4,17 +4,16 @@ from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import CommandStart
 
-# Настройка логирования
+
 logging.basicConfig(level=logging.INFO)
 
-# Токен бота (замените на ваш токен)
+
 BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
 
-# Создание объектов бота и диспетчера
+
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-# Основная функция запуска бота
 async def main():
     print("Бот запускается...")
     await dp.start_polling(bot)
