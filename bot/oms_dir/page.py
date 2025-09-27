96aa96cc493725322511cad54f777ade931fdf22
AS1 <arseniykofanov@gmail.com>
2025-09-27 15:00:28 +0300

    Получения ссылки на класс сценария и заготовки

--- bot/oms_dir/page.py
+++ bot/oms_dir/page.py
@@ -3,11 +3,14 @@ from typing import TYPE_CHECKING, Optional, Type
 from bot.oms_dir.models.scene import ScenePage, SceneModel, SceneLoader
 from aiogram.types import Message, CallbackQuery
 
+if TYPE_CHECKING:
+    from bot.oms_dir.scene import Scene
+
 class Page:
 
     __page_name__: str = ''
 
-    def __init__(self, scene: SceneModel, page_name: str = ''):
+    def __init__(self, scene: SceneModel, page_name: str = '', this_scene = None):
         if page_name and not self.__page_name__: 
             self.__page_name__ = page_name
 
@@ -22,9 +25,17 @@ class Page:
         self.content = self.__page__.content
         self.to_pages = self.__page__.to_pages
 
+        self.scene: Optional[Scene] = this_scene
+
     def __call__(self, *args, **kwargs):
         # self.__init__(*args, **kwargs)
         return self
+
+    def get_pagedata(self):
+        pass
+
+    def update_pagedata(self):
+        pass
 
     async def content_worker(self) -> str:
         return self.content
