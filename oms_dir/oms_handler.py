bd06532d820f4de97d29a1664a27ab4dd744d1cd
AS1 <arseniykofanov@gmail.com>
2025-09-27 18:29:06 +0300

    Обработчики вынесены в функцию с аргументом роутера

---
+++ oms_dir/oms_handler.py
@@ -0,0 +1,59 @@
+from typing import Type
+from aiogram.types import Message, CallbackQuery
+
+from oms_dir.models.scene import manager, CALLBACK_PREFIX
+from aiogram import F, Router
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+def register_handlers(router: Type[Router]):
+
+    @router.message()
+    async def on_message(message: Message):
+        user_id = message.from_user.id
+        scene = manager.get_scene(user_id)
+
+        logger.info(
+            f'on_message\nscene: {scene}\nmessage: {message.text}'
+        )
+
+        if scene:
+            await scene.text_handler(message)
+
+    @router.callback_query(
+        F.data.split(":")[:2] == [CALLBACK_PREFIX, 'to_page']
+                    )
+    async def to_page(callback: CallbackQuery):
+        user_id = callback.from_user.id
+        user_session = manager.get_scene(user_id)
+
+        prefix, c_type, scene_name, *args = callback.data.split(':')
+        to_page = args[0]
+
+        if user_session:
+            await user_session.update_page(to_page)
+
+    # @router.callback_query(
+    #     F.data.split(":")[:2] == [CALLBACK_PREFIX, 'scene_data']
+    #                 )
+    # async def scene_data(callback: CallbackQuery):
+    #     user_id = callback.from_user.id
+    #     user_session = manager.get_scene(user_id)
+
+    #     prefix, c_type, scene_name, *args = callback.data.split(':')
+    #     to_page = args[0]
+
+        # if user_session:
+        #     # await user_session.update_page(to_page)
+
+    @router.callback_query(
+        F.data.split(":")[0] == CALLBACK_PREFIX)
+    async def on_callback_query(callback: CallbackQuery):
+        user_id = callback.from_user.id
+        user_session = manager.get_scene(user_id)
+
+        prefix, c_type, scene_name, *args = callback.data.split(':')
+
+        if user_session:
+            await user_session.callback_handler(callback, args)
