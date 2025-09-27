a28b7b69b8cbb7cc3cd314274dfd63bbdc763068
AS1 <arseniykofanov@gmail.com>
2025-09-27 19:21:52 +0300

    Чистка

--- bot/handlers/test.py
+++ bot/handlers/test.py
@@ -1,15 +1,16 @@
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
-from bot.main import dp
-from bot.oms_dir.scene import manager
+from bot.main import dp, mainbot
+from oms import scene_manager
 from bot.scenes.user_task.task_scene import TaskScene
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message):
     user_id = message.from_user.id
-    scene = manager.create_scene(user_id,
-                                 TaskScene)
+    scene = scene_manager.create_scene(user_id,
+                                 TaskScene,
+                                 mainbot)
 
     if scene:
         await scene.start()
--- main.py
+++ main.py
@@ -2,8 +2,3 @@ from bot.main import run
 
 if __name__ == '__main__':
     run()
-
-# from bot.oms_dir.scene import manager
-# from bot.scenes.user_task.task_scene import TaskScene
-
-# manager.create_scene(123, TaskScene)
