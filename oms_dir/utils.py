4a309886db3fa251c45bdfa74133b50c2e8a27ec
AS1 <arseniykofanov@gmail.com>
2025-09-27 18:29:31 +0300

    Калбек генератор вынесен и добавлена функция для запуска синк и асинк функций

---
+++ oms_dir/utils.py
@@ -0,0 +1,50 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import inspect
+import asyncio
+
+CALLBACK_PREFIX = 'scene'
+CALLBACK_SEPARATOR = ':'
+
+def list_to_inline(buttons, row_width=3):
+    """ Преобразует список кнопок в inline-клавиатуру 
+         Example:
+              buttons = [ {'text': 'Кнопка 1', 'callback_data': 'btn1'},
+                          {'text': 'Кнопка 2', 'callback_data': 'btn2'},
+                          {'text': 'Кнопка 3', 'callback_data': 'btn3'} ]
+              
+              > Кнопка 1 | Кнопка 2 | Кнопка 3
+    """
+
+    inline_keyboard = []
+    row = []
+    for button in buttons:
+
+        if 'ignore_row' in button and (button['ignore_row'].lower() == 'true' or button['ignore_row'] == True):
+            inline_keyboard.append(row)
+            row = []
+            inline_keyboard.append([InlineKeyboardButton(**button)])
+            continue
+
+        row.append(InlineKeyboardButton(**button))
+        if len(row) == row_width:
+            inline_keyboard.append(row)
+            row = []
+    if row:
+        inline_keyboard.append(row)
+    return InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
+
+def callback_generator(scene_name: str, c_type: str, *args):
+    """ prefix:type:name:*args
+    """
+    sep = CALLBACK_SEPARATOR
+    return f'{CALLBACK_PREFIX}{sep}{c_type}{sep}{scene_name}{sep}{":".join(args)}'
+
+def use_inspect(func: callable, **kwargs) -> dict:
+    """ Функция для вызова функции с учётом того, асинхронная она или нет
+        kwargs - аргументы для передачи в функцию
+    """
+
+    if inspect.iscoroutinefunction(func):
+        return asyncio.create_task(func(**kwargs))
+    else:
+        return func(**kwargs)
