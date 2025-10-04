b090d9742fb71a416a8d266bfcda5e069a23fc19
AS1 <arseniykofanov@gmail.com>
2025-10-04 20:54:05 +0300

    -

--- bot/scenes/user_task/task_scene.py
+++ bot/scenes/user_task/task_scene.py
@@ -1,9 +1,7 @@
 from oms import Scene
-from bot.scenes.user_task.channels_page import ChannelsPage
 
 class TaskScene(Scene):
 
     __scene_name__ = 'user-task'
     __pages__ = [
-        ChannelsPage
     ]
